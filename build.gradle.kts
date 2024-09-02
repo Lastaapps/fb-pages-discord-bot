655a963216dd0007ac25abf852fec1fcca8979ec
Lasta Apps <krasik.peta@seznam.cz>
2024-09-02 23:17:05 +0300

    build: Deps bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     application
-    kotlin("jvm") version "2.0.0"
+    kotlin("jvm") version "2.0.20"
     id("org.jlleitschuh.gradle.ktlint") version "12.1.1"
     id("com.github.johnrengelman.shadow") version "8.1.1"
 }
@@ -23,7 +23,7 @@ repositories {
 
 // I'm to lazy to setup catalogs
 dependencies {
-    implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.6.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.6.1")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.9.0-RC")
 
     val kordVersion = "0.14.0"
@@ -33,14 +33,14 @@ dependencies {
 
     implementation("it.skrape:skrapeit:1.3.0-alpha.2")
     // fix security vulnerabilities in skrapeit libs
-    implementation("ch.qos.logback:logback-core:1.4.14")
-    implementation("ch.qos.logback:logback-classic:1.4.14")
-    implementation("commons-net:commons-net:3.9.0")
-    implementation("org.apache.commons:commons-text:1.10.0")
-    implementation("org.jsoup:jsoup:1.15.3")
+    implementation("ch.qos.logback:logback-core:1.5.7")
+    implementation("ch.qos.logback:logback-classic:1.5.7")
+    implementation("commons-net:commons-net:3.11.1")
+    implementation("org.apache.commons:commons-text:1.12.0")
+    implementation("org.jsoup:jsoup:1.18.1")
     implementation("xalan:xalan:2.7.3")
 
-    val ktorVersion = "2.3.11"
+    val ktorVersion = "2.3.12"
     implementation("io.ktor:ktor-client-core:$ktorVersion")
     implementation("io.ktor:ktor-client-cio:$ktorVersion")
 
@@ -49,7 +49,7 @@ dependencies {
     implementation("io.arrow-kt:arrow-fx-coroutines")
 
     testImplementation(kotlin("test"))
-    val kotestVersion = "5.9.0"
+    val kotestVersion = "5.9.1"
     testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
     testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
 }
