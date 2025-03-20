ec6bcaee75bf80ed8138baf3e91ebfba95810737
Lasta Apps <krasik.peta@seznam.cz>
2025-03-20 11:36:57 +0200

    refactor: Separate API retries

--- src/main/kotlin/cz/lastaapps/api/domain/error/Util.kt
+++ src/main/kotlin/cz/lastaapps/api/domain/error/Util.kt
@@ -11,6 +11,7 @@ import io.ktor.server.response.respond
 import io.ktor.server.routing.RoutingCall
 import kotlin.random.Random
 import kotlin.time.Duration
+import kotlin.time.Duration.Companion.minutes
 import kotlin.time.Duration.Companion.seconds
 
 private val log = Logger.withTag("catchingNetwork")
@@ -51,16 +52,39 @@ suspend fun <T> catchingDiscord(
 ): Outcome<T> = catchingNetwork(block)
 
 suspend fun <T> catchingFacebookAPI(
-    reoccurs: Long = 6,
-    initialDelay: Duration = 10.seconds, // 10, 20, 40, 1:20, 2:40, 5:20 | 10:40 21:20
     callID: Int = Random.nextInt(1000),
     block: suspend Raise<DomainError>.() -> T,
-): Outcome<T> = Schedule
-    // up to k retries -> k+1 in total
+): Outcome<T> =
+    timeoutSchedule(callID).or(
+        rateLimitSchedule(callID),
+        transform = transformNulls { o1, o2 ->
+            maxOf(o1.first, o2.first) to maxOf(o1.second, o2.second)
+        },
+        combineDuration = transformNulls { o1, o2 ->
+            maxOf(o1, o2)
+        },
+    )
+    .retryEither {
+        log.v { "Performing recoverable network call ($callID)" }
+        catchingNetwork(block)
+    }
+
+private fun <T : Any> transformNulls(transform: (T, T) -> T): (T?, T?) -> T =
+    { a: T?, b: T? ->
+        when {
+            a == null -> b!!
+            b == null -> a
+            else -> transform(a, b)
+        }
+    }
+
+private fun timeoutSchedule(
+    callID: Int,
+    reoccurs: Long = 6,
+    initialDelay: Duration = 10.seconds, // 10, 20, 40, 1:20, 2:40, 5:20 | 10:40 21:20
+) = Schedule
     .recurs<DomainError>(reoccurs)
-    // has to be long so rate limit is not reached
     .and(Schedule.exponential(initialDelay))
-    // retry if a timeout exception occurred
     .doWhile { input, (retryIndex, _) ->
         when (input) {
             is NetworkError.Timeout -> {
@@ -68,20 +92,27 @@ suspend fun <T> catchingFacebookAPI(
                 true
             }
 
+            else -> false
+        }
+    }
+
+private fun rateLimitSchedule(
+    callID: Int,
+    reoccurs: Long = 6,
+    initialDelay: Duration = 1.minutes,
+) = Schedule
+    .recurs<DomainError>(reoccurs)
+    .and(Schedule.exponential(initialDelay))
+    .jittered()
+    .doWhile { input, (retryIndex, _) ->
+        when (input) {
             is NetworkError.FBAPIError if input.isRateLimit -> {
                 log.w { "Retrying network call ($callID) because of rate limit (retry: $retryIndex)" }
                 true
             }
 
-            else -> {
-                log.v { "Retry skipped for call ($callID) as error is ${input::class.simpleName}" }
-                false
-            }
+            else -> false
         }
-    }
-    .retryEither {
-        log.v { "Performing recoverable network call ($callID)" }
-        catchingNetwork(block)
     }
 
 suspend inline fun RoutingCall.respondError(error: DomainError) =
